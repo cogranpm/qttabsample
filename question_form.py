85fee7678571f2d8d44c617faffa15f90343b3f0
paul martin <mussym@live.com>
2020-02-03 12:32:27 -0600

    table view

--- question_form.py
+++ question_form.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'question_form.ui',
 # licensing of 'question_form.ui' applies.
 #
-# Created: Mon Feb  3 11:07:49 2020
+# Created: Mon Feb  3 12:18:42 2020
 #      by: pyside2-uic  running on PySide2 5.13.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -14,35 +14,33 @@ class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(539, 471)
-        self.listView = QtWidgets.QListView(Form)
-        self.listView.setGeometry(QtCore.QRect(20, 10, 256, 188))
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(2)
-        sizePolicy.setHeightForWidth(self.listView.sizePolicy().hasHeightForWidth())
-        self.listView.setSizePolicy(sizePolicy)
-        self.listView.setObjectName("listView")
         self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(21, 203, 55, 17))
+        self.label.setGeometry(QtCore.QRect(20, 319, 55, 17))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(Form)
-        self.label_2.setGeometry(QtCore.QRect(21, 234, 24, 17))
+        self.label_2.setGeometry(QtCore.QRect(20, 350, 24, 17))
         self.label_2.setObjectName("label_2")
         self.txtBody = QtWidgets.QLineEdit(Form)
-        self.txtBody.setGeometry(QtCore.QRect(82, 203, 441, 25))
+        self.txtBody.setGeometry(QtCore.QRect(80, 320, 431, 25))
         self.txtBody.setObjectName("txtBody")
-        self.txtTag = QtWidgets.QLineEdit(Form)
-        self.txtTag.setGeometry(QtCore.QRect(82, 234, 108, 25))
-        self.txtTag.setObjectName("txtTag")
         self.txtAnswer = QtWidgets.QLineEdit(Form)
-        self.txtAnswer.setGeometry(QtCore.QRect(82, 265, 441, 25))
+        self.txtAnswer.setGeometry(QtCore.QRect(80, 382, 421, 25))
         self.txtAnswer.setObjectName("txtAnswer")
         self.label_3 = QtWidgets.QLabel(Form)
-        self.label_3.setGeometry(QtCore.QRect(21, 265, 46, 17))
+        self.label_3.setGeometry(QtCore.QRect(20, 381, 46, 17))
         self.label_3.setObjectName("label_3")
         self.btnAdd = QtWidgets.QPushButton(Form)
-        self.btnAdd.setGeometry(QtCore.QRect(430, 310, 80, 25))
+        self.btnAdd.setGeometry(QtCore.QRect(429, 426, 80, 25))
         self.btnAdd.setObjectName("btnAdd")
+        self.cboTags = QtWidgets.QComboBox(Form)
+        self.cboTags.setGeometry(QtCore.QRect(80, 351, 201, 25))
+        self.cboTags.setObjectName("cboTags")
+        self.questionView = QtWidgets.QTableView(Form)
+        self.questionView.setGeometry(QtCore.QRect(20, 40, 491, 261))
+        self.questionView.setObjectName("questionView")
+        self.label_4 = QtWidgets.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(30, 10, 91, 17))
+        self.label_4.setObjectName("label_4")
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -53,4 +51,5 @@ class Ui_Form(object):
         self.label_2.setText(QtWidgets.QApplication.translate("Form", "Tag:", None, -1))
         self.label_3.setText(QtWidgets.QApplication.translate("Form", "Answer:", None, -1))
         self.btnAdd.setText(QtWidgets.QApplication.translate("Form", "Add", None, -1))
+        self.label_4.setText(QtWidgets.QApplication.translate("Form", "Questions", None, -1))
 
--- question_form_view.py
+++ question_form_view.py
@@ -1,22 +1,62 @@
-from PySide2.QtCore import Signal, Slot
-from PySide2.QtGui import QBrush, QPen, QFont, QShowEvent, QResizeEvent
+from PySide2.QtCore import Signal, Slot, Qt, QAbstractTableModel, QModelIndex
+from PySide2.QtGui import QBrush, QPen, QFont, QShowEvent, QResizeEvent, QColor
 from PySide2.QtWidgets import QWidget, QMessageBox, QGraphicsScene, QGraphicsItem
-from PySide2.QtCore import Qt
+
+
 from question_form import Ui_Form
 
 import dataset
 
+
+class question_model(QAbstractTableModel):
+
+    def __init__(self, data=None):
+        QAbstractTableModel.__init__(self)
+        self.load_data(data)
+
+    def load_data(self, data):
+        self.bodys = data[0].values
+        self.tags = data[1].values
+        self.answers = data[2].values
+        self.column_count = 3
+        self.row_count = 0
+
+    def rowCount(self, parent=QModelIndex):
+        return self.row_count
+
+    def columnCount(self, parent=QModelIndex):
+        return self.column_count
+
+    def headerData(self, section, orientation, role):
+        if role != Qt.DisplayRole:
+            return None
+        if orientation == Qt.Horizontal:
+            return ("Body", "Tag", "Answer")[section]
+        else:
+            return "{}".format(section)
+
+    def data(self, index, role=Qt.DisplayRole):
+        column = index.column()
+        row = index.row()
+        # more stuff
 
 class question_form_view(QWidget):
     def __init__(self):
         QWidget.__init__(self)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        self.ui.btnAdd.clicked.connect(self.add_click)
+        tags = ['collections', 'other', 'functional']
+        for tag in tags:
+            self.ui.cboTags.addItem(tag, tag)
+        data = [{'body':'something', 'tag':'collections', 'answer':'answer'}]
 
-    def showEvent(self, event: QShowEvent):
+        self.model = question_model(data)
+        self.ui.questionView.setModel(self.model)
+        self.ui.btnAdd.clicked.connect(self.add_click)
         self.db = dataset.connect('sqlite:///kernai.db')
         self.questions = self.db['questions']
+
+    def showEvent(self, event: QShowEvent):
         for question in self.db['questions']:
             print(question['body'])
             print(question['answer'])
@@ -24,8 +64,9 @@ class question_form_view(QWidget):
     def add_click(self):
         new_question = {}
         new_question["body"] = self.ui.txtBody.text()
-        new_question["tag"] = self.ui.txtTag.text()
+        new_question["tag"] = self.ui.cboTags.itemData(self.ui.cboTags.currentIndex())
         new_question["answer"] = self.ui.txtAnswer.text()
+        print(new_question)
         self.questions.insert(new_question)
 
 
