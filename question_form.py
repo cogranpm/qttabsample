d0e27a0f4ea6bb2700bda6bbb2d3d9e6df92087d
paul martin <mussym@live.com>
2020-02-25 16:45:15 -0600

    ui rebuilt

--- question_form.py
+++ question_form.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'question_form.ui',
 # licensing of 'question_form.ui' applies.
 #
-# Created: Mon Feb 24 11:30:38 2020
+# Created: Mon Feb 24 22:10:08 2020
 #      by: pyside2-uic  running on PySide2 5.13.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -14,28 +14,22 @@ class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(664, 591)
-        self.btnAdd = QtWidgets.QPushButton(Form)
-        self.btnAdd.setGeometry(QtCore.QRect(90, 10, 80, 25))
-        self.btnAdd.setObjectName("btnAdd")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.label_4 = QtWidgets.QLabel(Form)
-        self.label_4.setGeometry(QtCore.QRect(9, 9, 58, 17))
         self.label_4.setObjectName("label_4")
-        self.btnSave = QtWidgets.QPushButton(Form)
-        self.btnSave.setGeometry(QtCore.QRect(560, 550, 80, 25))
-        self.btnSave.setObjectName("btnSave")
-        self.txtID = QtWidgets.QLineEdit(Form)
-        self.txtID.setGeometry(QtCore.QRect(60, 550, 113, 25))
-        self.txtID.setObjectName("txtID")
-        self.widget = QtWidgets.QWidget(Form)
-        self.widget.setGeometry(QtCore.QRect(10, 40, 631, 498))
-        self.widget.setObjectName("widget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.widget)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.addWidget(self.label_4)
+        self.btnAdd = QtWidgets.QPushButton(Form)
+        self.btnAdd.setObjectName("btnAdd")
+        self.verticalLayout_2.addWidget(self.btnAdd)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
-        self.questionView = QtWidgets.QTableView(self.widget)
+        self.questionView = QtWidgets.QTableView(Form)
+        self.questionView.setAlternatingRowColors(True)
+        self.questionView.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
         self.questionView.setObjectName("questionView")
         self.verticalLayout.addWidget(self.questionView)
-        self.groupBox = QtWidgets.QGroupBox(self.widget)
+        self.groupBox = QtWidgets.QGroupBox(Form)
         self.groupBox.setObjectName("groupBox")
         self.formLayout = QtWidgets.QFormLayout(self.groupBox)
         self.formLayout.setObjectName("formLayout")
@@ -58,17 +52,24 @@ class Ui_Form(object):
         self.txtAnswer.setObjectName("txtAnswer")
         self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.txtAnswer)
         self.verticalLayout.addWidget(self.groupBox)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+        self.txtID = QtWidgets.QLineEdit(Form)
+        self.txtID.setObjectName("txtID")
+        self.verticalLayout_2.addWidget(self.txtID)
+        self.btnSave = QtWidgets.QPushButton(Form)
+        self.btnSave.setObjectName("btnSave")
+        self.verticalLayout_2.addWidget(self.btnSave)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QtWidgets.QApplication.translate("Form", "Form", None, -1))
-        self.btnAdd.setText(QtWidgets.QApplication.translate("Form", "Add", None, -1))
         self.label_4.setText(QtWidgets.QApplication.translate("Form", "Questions", None, -1))
-        self.btnSave.setText(QtWidgets.QApplication.translate("Form", "Save", None, -1))
+        self.btnAdd.setText(QtWidgets.QApplication.translate("Form", "Add", None, -1))
         self.groupBox.setTitle(QtWidgets.QApplication.translate("Form", "Edit", None, -1))
         self.label.setText(QtWidgets.QApplication.translate("Form", "Question:", None, -1))
         self.label_2.setText(QtWidgets.QApplication.translate("Form", "Tag:", None, -1))
         self.label_3.setText(QtWidgets.QApplication.translate("Form", "Answer:", None, -1))
+        self.btnSave.setText(QtWidgets.QApplication.translate("Form", "Save", None, -1))
 
